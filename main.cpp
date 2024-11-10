5b0f71a48b811780069711f94ed8cf9e520272e0
JaredMel <jareddmelendez1764@gmail.com>
2024-11-10 11:47:05 -0800

    update 4

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ void simulateGame(map<string, vector<list<string>>> &gameCollection, vector<stri
     {
         cout << "Year #" << i+1 << ":" << endl;
         // Iterate through each genre in the map
-        for (auto x : gameCollection)
+        for (auto x = gameCollection.begin(); x != gameCollection.end(); x++)
         {
             for (size_t j = 0; j < countGenres; j++)
             {
@@ -92,22 +92,22 @@ void simulateGame(map<string, vector<list<string>>> &gameCollection, vector<stri
                 if (prob <= 50)
                 {
                     ran = rand() % 99;
-                    x.second[j].push_back(gamesVec[ran]);
-                    cout << "Bought " << x.second[j].back() << " for " << genre[j] << " in " << x.first << " condition" << endl;
+                    x->second[j].push_back(gamesVec[ran]);
+                    cout << "Bought " << x->second[j].back() << " for " << genre[j] << " in " << x->first << " condition" << endl;
                     prob = rand() % 100 + 1;
                 }
                 // If selling, select a random game from the list to remove
-                if (prob <= 20 && x.second[j].size() > 0)
+                if (prob <= 20 && x->second[j].size() > 0)
                 {
-                    cout << "Sold " << x.second[j].front() << " for " << genre[j] << " in " << x.first << " condition" << endl;
-                    x.second[j].pop_front();
+                    cout << "Sold " << x->second[j].front() << " for " << genre[j] << " in " << x->first << " condition" << endl;
+                    x->second[j].pop_front();
                     prob = rand() % 100 + 1;
                 }
                 // If returning, select a random game from the list to remove only with a different message
-                if (prob <= 10 && x.second[j].size() > 0)
+                if (prob <= 10 && x->second[j].size() > 0)
                 {
-                    cout << "Returned " << x.second[j].back() << " for " << genre[j] << " in " << x.first << " condition" << endl;
-                    x.second[j].pop_back();
+                    cout << "Returned " << x->second[j].back() << " for " << genre[j] << " in " << x->first << " condition" << endl;
+                    x->second[j].pop_back();
                     prob = rand() % 100 + 1;
                 }
             }
